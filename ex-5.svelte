97cdd5045625f7dfea4c3a37654b435742d93abf
Ana Carolina Baptista <119694053+carolinabaptist@users.noreply.github.com>
2024-12-01 20:00:16 +0000

    Create ex-5.svelte

---
+++ ex-5.svelte
@@ -0,0 +1,21 @@
+<script>
+	function handleClick(e) {
+		alert("I've been clicked!!!")
+	}
+</script>
+
+<button on:click={handleClick}>
+  Press me
+</button>
+    
+<button on:click={() => console.log('I was pressed')}>
+  Press me
+</button>
+
+<button on:click|once={handleClick}>
+  Press me once!!!
+</button>
+
+<button on:submit|preventDefault={handleClick}>
+  Press me
+</button>
