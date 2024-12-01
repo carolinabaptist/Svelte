333f60e43c928982fe5597fd418f9194084fad88
Ana Carolina Baptista <119694053+carolinabaptist@users.noreply.github.com>
2024-12-01 17:19:13 +0000

    Create App.svelte

---
+++ ex-3/App.svelte
@@ -0,0 +1,11 @@
+<script>
+  import MyLink from "./MyLink.svelte";
+  let link = {
+    href: "http://www.mysite.com",
+    title: "My Site",
+    link: "#",
+    color: "#ff3300"
+  };
+</script>
+
+<MyLink {...link} />
