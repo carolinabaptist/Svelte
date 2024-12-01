ce14a0f029da21f27d21b49c9a15717cd07f2dde
Ana Carolina Baptista <119694053+carolinabaptist@users.noreply.github.com>
2024-12-01 19:25:11 +0000

    Update Question.svelte

--- ex-4/Question.svelte
+++ ex-4/Question.svelte
@@ -1,5 +1,16 @@
 <script>
   export let question;
+
+  let answers = question.incorrect_answers;
+  
 </script>
 
-<h3>{question.question}</h3>
+<h3>{@html question.question}</h3>
+
+{#each answers as answer}
+
+    <button> {@html answer} </button>
+
+{/each}
+
+
